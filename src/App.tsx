9a7534ffd28cb9012ba638c59d4e3e99ad02c818
EmmaPorras <tuemail@dominio.com>
2025-04-11 20:15:03 -0600

    Funcion_delete

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 interface Todo {
- // id: number sdasdasdasdasdasd
+ // id: number 
   description: string
   //isDone: boolean
 }
@@ -11,10 +11,10 @@ function App() {
   const [todoDescription, setTodoDescription] = useState('')
   //const [todoList, setTodoList] = useState<Todo[]>([])
 
+  // Inicializa la lista desde localStorage, si existe
   const [todoList, setTodoList] = useState<Todo[]>(() => {
-  // Recuperar datos del Local Storage al inicializar el estado
-  const savedTodos = localStorage.getItem('todoList')
-  return savedTodos ? JSON.parse(savedTodos) : []
+    const savedTodos = localStorage.getItem('todoList')
+    return savedTodos ? JSON.parse(savedTodos) : []
 })
 
 
@@ -47,6 +47,13 @@ function App() {
     setTodoDescription('') // Limpiar el input después de agregar
   }
 
+ // Esta función elimina una tarea por su índice
+ const handleDelete = (indexToDelete: number) => {
+  const updatedList = todoList.filter((_, index) => index !== indexToDelete)
+  setTodoList(updatedList)
+ }
+
+
   return (
     <>
     <div style={{border: '1px solid red', padding: 10}}>
@@ -54,18 +61,30 @@ function App() {
         <input
         value = {todoDescription} 
         onChange={handleChange}
-        style = {{marginRight: 10}}/>
+        style = {{marginRight: 10}}
+        />
         <button onClick={handleClick}>Add Item</button>
       </div>
+      
       <div>TODOS Here</div>
       <ul>
-        {todoList.map((todo, index) => {
-          return <li key={index}>
-            <input type="checkbox"/>
-            {todo.description}</li>
-        })}
-      </ul>
-    </div>
+      {todoList.map((todo, index) => {
+            return (
+              <li key={index}>
+                <input type="checkbox" />
+                {todo.description}
+                {/* Botón de eliminar tarea */}
+                <button
+                  onClick={() => handleDelete(index)}
+                  style={{ marginLeft: 10, color: 'white', backgroundColor: 'grey' }}
+                >
+                  Delete
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </>
   )
 }
